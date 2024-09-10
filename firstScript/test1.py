8ef1bea67072876bb12dd4fb234d78f88c1eae64
Emtiaz Chowdhury <emtiazchow@gmail.com>
2024-09-09 23:01:04 -0400

    first commit

---
+++ firstScript/test1.py
@@ -0,0 +1,16 @@
+#Print in Python
+print('hello world!')
+
+#Data Type in Python: Python understands data type automatically based on the data. There is no need to declare it explicitly.
+age = 25 #Integer.
+pi = 3.5 # Float.
+name = 'emtiaz' #String. single/double quote both acceptable.
+is_Active = True #Boolean. True/false not true/false.
+
+#List: we can can access add and remove
+fruits = []
+
+#Tuple
+
+
+#Dictionary
